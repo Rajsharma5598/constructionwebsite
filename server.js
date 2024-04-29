a8239b22ca3e9c5f6cea454451c502f0f13bee32
Raj Kumar Sharma 12222255 <119494217+Rajsharma5598@users.noreply.github.com>
2024-04-30 00:32:03 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+// server.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const path = require('path');
+
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Connect to MongoDB (replace 'your-mongodb-uri' with your actual MongoDB connection string)
+mongoose.connect('mongodb+srv://rajsharma55987:12345@cluster0.nwnzdak.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define a schema for the data
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("form\public"));
+
+// Serve the HTML form
+app.get('/', (req, res) => {
+  // res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'ind.html'));
+});
+
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Handle form submission
+app.post('/submit', async(req, res) => {
+  try {
+    const { name, email, message } = req.body;
+    const newUser = new User({ name, email, message });
+    await newUser.save();
+    res.send('Data saved successfully!');
+  } catch (error) {
+    res.status(500).send('Error saving data.');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
